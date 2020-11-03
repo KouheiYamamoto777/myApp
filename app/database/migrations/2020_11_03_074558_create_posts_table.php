48e21b98f35589c55e66cc7f819888da64fb015f
KouheiYamamoto777 <kouheiyamamoto777@gmail.com>
2020-11-03 17:08:32 +0900

    マイグレーションファイル作成
    現在構想中のデータベース構成を元にマイグレーションファイル作成/マイグレーション実行までしました。
    
    usersテーブル
    ユーザー管理
    
    postsテーブル
    記録をつけた時のタイトルとメッセージ、記録年、記録月が保存される.これを元に投稿が管理される
    
    incomesテーブル
    記録をつけた時の金額と記録年、記録月が保存される.グラフに表示するデータを保存する
    
    profilesテーブル
    ユーザーのプロフィール情報が保存される.コメント、プロフィール画像、クレジットカードの上限を保存する
    
    commentテーブル
    投稿に対する別ユーザーのコメントを保存する.

---
+++ app/database/migrations/2020_11_03_074558_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('posts'))
+        {
+            Schema::create('posts', function (Blueprint $table) {
+                $table->id();
+                $table->integer('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->string('title');
+                $table->text('body');
+                $table->tinyInteger('year');
+                $table->tinyInteger('month');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ app/database/migrations/2020_11_03_075233_create_comment_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('comment'))
+        {
+            Schema::create('comment', function (Blueprint $table) {
+                $table->id();
+                $table->integer('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->integer('post_id');
+                $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+                $table->string('comment');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
