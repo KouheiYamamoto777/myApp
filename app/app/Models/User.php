18396ef794989e84fc86f9824979703dc33bf803
KouheiYamamoto777 <kouheiyamamoto777@gmail.com>
2020-11-21 23:06:08 +0900

    スキーマ定義を追記しました。 各リレーションの定義を決定し、動作の確認が完了しました。

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -61,7 +61,7 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class);
     }
 
-    public function income(): HasMany
+    public function incomes(): HasMany
     {
         return $this->hasMany(Income::class);
     }
